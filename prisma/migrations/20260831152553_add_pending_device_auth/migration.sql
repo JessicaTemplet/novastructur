7f80e0d42b381c807691138f9c4ed66175615994
Jessica Templet <jessift82@gmail.com>
2026-09-04 06:03:24 -0500

    Remove stale SQLite migration (schema is now Postgres)

--- prisma/migrations/20260831152553_add_pending_device_auth/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "PendingDeviceAuth" (
-    "userId" TEXT NOT NULL PRIMARY KEY,
-    "deviceCode" TEXT NOT NULL,
-    "interval" INTEGER NOT NULL,
-    "expiresAt" DATETIME NOT NULL,
-    CONSTRAINT "PendingDeviceAuth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
